ec08f920e576b08ba326055153f8c73aade24ec6
ajBFW <an.jenett@bfw.koeln>
2016-10-05 10:20:23 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install -y htop nginx dnsmasq tftpd-hpa git
+sudo /etc/init.d/tftpd-hpa stop
 sudo update-rc.d -f tftpd-hpa remove
 
 git clone https://github.com/ajBFW/Tankstelle
