f81f0288d440b0fe2a800e55e583bc7055c2075c
ajBFW <an.jenett@bfw.koeln>
2016-10-05 19:27:01 +0200

    changed

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo apt-get install -y htop nginx dnsmasq tftpd-hpa git
-sleep 15
+sleep 45
 sudo /etc/init.d/tftpd-hpa stop
 sudo update-rc.d -f tftpd-hpa remove
 git clone https://github.com/ajBFW/Tankstelle
-sleep 20
+sleep 45
 sudo cp Tankstelle/dnsmasq.conf /etc/
 sudo cp -r Tankstelle/tftp/ /srv/
 sudo service dnsmasq restart
